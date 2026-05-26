b06b0cc721f5b473ba9297c837261ef6c908d9a4
Sai Sujan <saisujan2707@gmail.com>
2026-05-26 15:33:32 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://shopsavyy.vercel.app'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
